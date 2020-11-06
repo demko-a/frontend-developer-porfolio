40eda354bda8fc21f02e223c1be39ac886fc4f49
demko-a <a.e.demko@mail.ru>
2020-11-06 14:53:31 +0300

    add show tooltip for project item

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const projectsList = document.querySelector('.projects_list');
+const projectContainer = document.querySelector('.project_container');
+
+let tooltipElem = null;
 
 const getData = async function(url) {
 	const response = await fetch(url);
@@ -27,7 +30,8 @@ function createProjectItem(project) {
     const innerHTML = `
     <div class="project_item_content">
         <a class="projects_link" 
-            href=${host}>
+            href=${host}
+            data-tooltip="${description}">
             ${name}
         </a>
         <span>
@@ -39,4 +43,41 @@ function createProjectItem(project) {
     projectsList.insertAdjacentElement('beforeend', projectItem);
 }
 
-getData('./db/projects.json').then(data => data.forEach(createProjectItem));
+function calcTooltipCoords(target) {
+    const targetCoords = target.getBoundingClientRect();
+    return {
+        top: (targetCoords.top + targetCoords.height),
+        left: targetCoords.left
+    };
+}
+
+function onMouseover(event) {
+    const tooltipContent = event.target.dataset.tooltip;
+    if (!tooltipContent) return;
+    if (tooltipElem) return;
+
+    tooltipElem = document.createElement('div');
+    tooltipElem.className = 'tooltip';
+    tooltipElem.innerHTML = tooltipContent;
+
+    const coords = calcTooltipCoords(event.target);
+
+    tooltipElem.style.top = coords?.top + 'px';
+    tooltipElem.style.left = coords?.left + 'px'
+
+    projectContainer.insertAdjacentElement('beforeend', tooltipElem);
+}
+
+function onMouseout() {
+    if (!tooltipElem) return;
+    tooltipElem.remove();
+    tooltipElem = null;
+}
+
+function init() {
+    getData('./db/projects.json').then(data => data.forEach(createProjectItem));
+    projectContainer.addEventListener('mouseover', onMouseover);
+    projectContainer.addEventListener('mouseout', onMouseout);
+}
+
+init();
